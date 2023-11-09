71e565a6cf005d824e1ca8fe8c48ad8bb237dfcf
jopzik <jmora@ia.com.mx>
2023-11-09 16:26:17 -0700

    Add Footer and Header files

---
+++ src/stories/Footer.stories.js
@@ -0,0 +1,11 @@
+import Footer from "../components/Footer";
+
+export default {
+  title: 'Organism/Navigation/Footer',
+  component: Footer,
+  parameters: {
+    layout: 'centered',
+  },
+}
+
+export const Default = {}
---
+++ src/stories/Header.stories.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+
+export default {
+  title: 'Organism/Navigation/Header',
+  component: Header,
+  parameters: {
+    layout: 'centered',
+  },
+}
+
+export const Default = {
+  args: {
+
+  }
+}
